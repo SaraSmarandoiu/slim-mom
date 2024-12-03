47cd4cbb7a0ce059abddb3c177a9a19104fd269e
Sara Smarandoiu <smarandoiusara@gmail.com>
2024-12-04 00:06:22 +0200

    fix

--- src/redux/auth.js
+++ src/redux/auth.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL || 'http://localhost:3001/api',
+    baseUrl: 'http://localhost:3001/api',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
 
