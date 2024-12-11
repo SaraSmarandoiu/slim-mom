4a317376dcf4c3c00560f629b867f23e6bbbaacc
Sara Smarandoiu <smarandoiusara@gmail.com>
2024-12-11 15:13:32 +0200

    c

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -15,9 +15,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <ThemeContextProvider >
         <PersistGate loading={null} persistor={persistor}>
-          <BrowserRouter basename="/slim-mom">
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </PersistGate>
       </ThemeContextProvider>
     </Provider>
